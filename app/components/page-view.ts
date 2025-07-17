a58ef9cf1f30198e0518fa15ff40eafa28204a2d
moransantiago <morangsantiago@gmail.com>
2025-07-17 13:50:50 -0300

    Fix import

--- app/components/page-view.ts
+++ app/components/page-view.ts
@@ -1,5 +1,5 @@
 'use client'
-import { Pageviews } from '@/basehub'
+import { Pageviews } from '@/basehub-types'
 import { sendEvent } from 'basehub/events'
 import { useEffect, useRef } from 'react'
 
