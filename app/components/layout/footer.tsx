fd557d52921ee16ecc65038d374f4bdbac0722d1
moransantiago <morangsantiago@gmail.com>
2025-07-17 13:48:00 -0300

    Use react-icon package now that it supports text nodes

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -1,3 +1,4 @@
+import { Icon } from 'basehub/react-icon'
 import { Pump } from 'basehub/react-pump'
 
 export const Footer = async () => {
@@ -12,17 +13,30 @@ export const Footer = async () => {
 
         return (
           <footer className="mt-auto w-full overflow-clip">
-            {/* <Icon content={footer.vector} /> */}
             {footer.vector && (
-              <span
-                dangerouslySetInnerHTML={{ __html: footer.vector }}
-                className="lowercase light-only text-dim translate-y-[24%] xl:translate-y-1/3 cursor-default block select-none w-[105vw] relative left-1/2 -translate-x-1/2 font-mono"
+              <Icon
+                content={footer.vector}
+                components={{
+                  svg: (props) => (
+                    <svg
+                      {...props}
+                      className="lowercase light-only text-dim translate-y-[24%] xl:translate-y-1/3 cursor-default block select-none w-[105vw] relative left-1/2 -translate-x-1/2 font-mono"
+                    />
+                  )
+                }}
               />
             )}
             {footer.vectorDark && (
-              <span
-                dangerouslySetInnerHTML={{ __html: footer.vectorDark }}
-                className="lowercase dark-only text-dim translate-y-[24%] xl:translate-y-1/3 cursor-default block select-none w-[105vw] relative left-1/2 -translate-x-1/2 font-mono"
+              <Icon
+                content={footer.vectorDark}
+                components={{
+                  svg: (props) => (
+                    <svg
+                      {...props}
+                      className="lowercase dark-only text-dim translate-y-[24%] xl:translate-y-1/3 cursor-default block select-none w-[105vw] relative left-1/2 -translate-x-1/2 font-mono"
+                    />
+                  )
+                }}
               />
             )}
           </footer>
